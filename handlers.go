e2f44618f3a56f95f9bbcbd5fe2841f47388e496
michaelboegner <cmikeb@gmail.com>
2024-09-14 14:35:55 -0400

    Hashing password on user creation and comparing hashed pass using bcrypt

--- handlers.go
+++ handlers.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type acceptedVals struct {
@@ -41,8 +44,14 @@ func (apiCfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	// POST create a user
 	case http.MethodPost:
-		_, err := apiCfg.DB.Exec("INSERT INTO users (username, password, email) VALUES ($1, $2, $3)", params.Username, params.Password, params.Email)
+		password, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcrypt.MinCost)
 		if err != nil {
+			log.Printf("Error: %v\n", err)
+		}
+
+		_, err = apiCfg.DB.Exec("INSERT INTO users (username, password, email) VALUES ($1, $2, $3)", params.Username, password, params.Email)
+		if err != nil {
+			log.Printf("Error: %v\n", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
@@ -86,12 +95,28 @@ func (apiCfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	// POST login a user
 	case http.MethodPost:
-		_, err := apiCfg.DB.Exec("SELECT (username, password) from users WHERE username = $1 and password = $2 ", params.Username, params.Password)
-		if err != nil {
+		var hashedPassword string
+		err = apiCfg.DB.QueryRow("SELECT password from users WHERE username = $1", params.Username).Scan(&hashedPassword)
+		if err == sql.ErrNoRows {
+			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+			return
+		} else if err != nil {
+			log.Printf("Error querying database: %v\n", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
+		err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(params.Password))
+		if err != nil {
+			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+			return
+		}
 	}
+
+	payload := returnVals{
+		Username: params.Username,
+	}
+
+	respondWithJSON(w, http.StatusOK, payload)
 }
 
 func getParams(r *http.Request, w http.ResponseWriter) (acceptedVals, error) {
