4087d50efceba339d30f3f10da9568acd08efb06
michaelboegner <cmikeb@gmail.com>
2025-03-25 13:52:15 +0700

    abstracted away test request logic to testRequests() helper

--- internal/testutil/helpers.go
+++ internal/testutil/helpers.go
@@ -2,52 +2,65 @@ package testutil
 
 import (
 	"io"
-	"log"
 	"net/http"
 	"strings"
 	"testing"
 )
 
 func CreateTestUserAndJWT(t *testing.T) (string, string, error) {
-
 	t.Helper()
-	client := &http.Client{}
 
-	reqBodyUser := `{
+	var (
+		user string
+	)
+
+	reqBodyUser := strings.NewReader(`{
 		"username":"test",
 		"email":"test@email.com",
 		"password":"test"
-	}`
+	}`)
 
-	t.Logf("Request body being sent: %s", reqBodyUser)
-	req, err := http.NewRequest("POST", TestServerURL+"/api/users/", strings.NewReader(reqBodyUser))
+	bodyBytes, err := testRequests(t, "POST", TestServerURL+"/api/users/", reqBodyUser)
 	if err != nil {
 		t.Logf("CreateTestUserAndJWT user creation failed: %v", err)
+	}
+
+	user = string(bodyBytes)
+
+	// 2. Login via POST /api/auth/login
+	reqBodyLogin := `
+		{
+			"username": "test",
+			"password": "test"
+		}
+	`
+
+	// 3. Extract JWT + userID from response
+
+	return user, "", nil
+}
+
+func testRequests(t *testing.T, method, url string, reqBody *strings.Reader) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest(method, TestServerURL+url, reqBody)
+	if err != nil {
+		t.Logf("CreateTestUserAndJWT user creation failed: %v", err)
+		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		t.Logf("Request to create test user failed: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Logf("Reading response failed: %v", err)
+		return nil, err
 	}
 
-	log.Printf("\n\n\nTHIS IS THE RESP: %v\n\n\n", string(bodyBytes))
-
-	// 2. Login via POST /api/auth/login
-	// reqBodyLogin := `
-	// 	{
-	// 		"username": "test",
-	// 		"password": "test"
-	// 	}
-	// `
-
-	// 3. Extract JWT + userID from response
-
-	return "", "", nil
+	return bodyBytes, nil
 }
