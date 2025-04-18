e1261b4f17004f199bb1aea84ffd81d846895eb2
michaelboegner <cmikeb@gmail.com>
2025-04-18 11:53:19 +0700

    Revert "cleaned up file structure. Removed old mock files as they were part of a previous version of integration testing for the handlers."
    This reverts commit 62d0b9833977964d1e904a4b8725db46001e7f9c.

---
+++ interview/repository_mock.go
@@ -0,0 +1,33 @@
+package interview
+
+import (
+	"time"
+)
+
+type MockRepo struct{}
+
+func NewMockRepo() *MockRepo {
+	return &MockRepo{}
+}
+
+func (repo *MockRepo) CreateInterview(interview *Interview) (int, error) {
+	id := 1
+	return id, nil
+}
+
+func (repo *MockRepo) GetInterview(interviewID int) (*Interview, error) {
+	interview := &Interview{
+		Id:              1,
+		UserId:          1,
+		Length:          30,
+		NumberQuestions: 5,
+		Difficulty:      "easy",
+		Status:          "running",
+		Score:           0,
+		Language:        "python",
+		FirstQuestion:   "What is the flight speed of an unladdened swallow?",
+		CreatedAt:       time.Now().UTC(),
+		UpdatedAt:       time.Now().UTC(),
+	}
+	return interview, nil
+}
---
+++ user/repository_mock.go
@@ -0,0 +1,45 @@
+package user
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type MockRepo struct {
+	Users map[int]User
+}
+
+func NewMockRepo() *MockRepo {
+	return &MockRepo{
+		Users: map[int]User{
+			1: {ID: 1, Username: "testuser", Password: []byte("$2a$10$...")},
+		},
+	}
+}
+
+func (m *MockRepo) CreateUser(user *User) (int, error) {
+	m.Users[0] = *user
+	return 1, nil
+}
+
+func (m *MockRepo) GetUser(user *User) (*User, error) {
+	mockUser := &User{
+		ID:       user.ID,
+		Username: "testuser",
+		Email:    "test@example.com",
+	}
+
+	return mockUser, nil
+}
+
+func (m *MockRepo) GetPasswordandID(username string) (int, string, error) {
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+		return 0, "", err
+	}
+
+	passwordString := string(passwordHashed)
+	return 1, passwordString, nil
+}
