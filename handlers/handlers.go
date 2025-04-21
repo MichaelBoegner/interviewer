5002137b63749cccf0442c1fe5b553353c3f855f
michaelboegner <cmikeb@gmail.com>
2025-04-21 12:34:08 +0700

    added GET method check to

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,6 +14,11 @@ import (
 )
 
 func (h *Handler) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("healthy"))
 	return
