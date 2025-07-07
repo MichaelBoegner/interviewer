08b91065ad9f38cdff86ddd261406d5432522f86
michaelboegner <cmikeb@gmail.com>
2025-07-07 12:41:28 +0700

    moving to new branch for embeddings testing

--- database/migrations/000010_add_pgvector_and_conversation_embeddings.down.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS conversation_embeddings_lookup_idx;
-DROP INDEX IF EXISTS conversation_embeddings_embedding_idx;
-DROP TABLE IF EXISTS conversation_embeddings;
-DROP EXTENSION IF EXISTS vector;
